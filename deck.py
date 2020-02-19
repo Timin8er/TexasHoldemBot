cab985a3b2da9c28388ccaf4f973e0e6359aba4f
Tim <botballmaster@hotmail.com>
2020-02-18 22:14:30 -0500

    Hand Rank introduced

--- deck.py
+++ deck.py
@@ -18,14 +18,33 @@ class Deck():
     Container for a deck of cards
     """
 
-    _d = [i for i in deckGenerator()]
-
     def __init__(self):
-        pass
-    
+        self._d = [i for i in deckGenerator()]
+
 
     def __len__(self):
         return len(self._d)
+
+
+    def pop(self, index1:int, index2:int = None):
+        """
+        pops and returns the card at index1 or the cards with suit=index1 and rank=index2
+        """
+        if index2 is None:
+            if index1 >= len(self._d):
+                raise ValueError('Deck index out of range: %s, max: %s' % (index1, len(self._d)))
+            return self._d.pop(index1)
+
+        # start at the initial index of the card
+        i = 13*index1 + index2
+        # while we havn't foind the card yet, decrement i
+        while i >= len(self._d) or self._d[i][0] != index1 or self._d[i][1] != index2:
+            i -= 1
+            if i < 0:
+                raise ValueError('Card Not Found: (%s, %s)' % (index1, index2))
+
+        # return the card at i
+        return self._d.pop(i)
 
 
     def pairs_generator(self):
@@ -34,9 +53,8 @@ class Deck():
 
     def pop_card(self):
         return self._d.pop(random.randrange(len(self._d)))
-    
+
 
     def return_card(self, card):
         if card not in self._d:
             self._d.append(card)
-
--- rawOdds/rawOddsTestor.py
+++ rawOdds/rawOddsTestor.py
@@ -3,6 +3,24 @@ from deck import *
 import random
 from itertools import combinations
 
+STREIGHT_FLUSH = 0
+FOUR_OF_A_KIND = 1
+FULL_HOUSE = 2
+FLUSH = 3
+STREIGHT = 4
+THREE_OF_A_KIND = 5
+TWO_PAIR = 6
+PAIR = 7
+HIGH_CARD_1 = 8
+HIGH_CARD_2 = 9
+
+
+test_cases = [
+    # [(0,9),(1,9)], # pair in hand
+    # [(0,6),(1,7),(2,8),(3,9),(0,10)], # streight
+    [(0,3),(0,6),(1,3),(2,3),(3,9)] # 3 of a kind
+]
+
 
 def test_odds_of_pair():
     """
@@ -16,7 +34,9 @@ def test_odds_of_pair():
     hand1 = deck.pop_card()
     hand2 = deck.pop_card()
 
-    test_result = rawOddsCalculator.odds_of_opponent_better_pair(river, hand1, hand2)
+    cards = [hand1, hand2] + river
+
+    print(has_hands(cards))
 
     # determine my pair
     my_pair_value = -1
@@ -45,7 +65,97 @@ def test_odds_of_pair():
 
 
 def test_everything(deck:Deck):
-    print(' ========== Testing doubles ========== ')
+    print(' ========== Testings ========== ')
 
-    test_odds_of_pair()
+    # test_odds_of_pair()
 
+    for t in test_cases:
+        deck = Deck()
+
+        my_cards = [deck.pop(*j) for j in t]
+        print(my_cards)
+        my_hand_r = hand_rank(my_cards)
+        print(my_hand_r)
+
+        # for each posible pair an apponent has
+        better_pairs = 0
+        for pairs_checked, opp_cards in enumerate(deck.pairs_generator()):
+
+            # get their hand_rank
+            hand_r = hand_rank(list(opp_cards) + my_cards[2:])
+
+            # for each index in hand_ranks
+            for i, j in zip(my_hand_r, hand_r):
+                # if my hand is better, break
+                if i > j:
+                    break
+                # if their hand is better break
+                elif i < j:
+                    better_pairs += 1
+                    break
+
+        print(better_pairs, '/', pairs_checked)
+
+
+
+
+def hand_rank(cards:list):
+
+    igot = [-1] * 10
+
+    # tally the number of each card rank and suit we have
+    rank_accumulations = [0] * 13
+    suit_accumilations = [0] * 4
+    for c in cards:
+        rank_accumulations[c[1]] += 1
+        suit_accumilations[c[0]] += 1
+
+    # tally all x of a kind
+    accumulation_counts = [0] * 5
+    for i, acc in enumerate(rank_accumulations):
+        accumulation_counts[acc] += 1
+
+        if acc == 4 and i > igot[FOUR_OF_A_KIND]:
+            igot[1] = i
+
+        elif acc == 3 and i > igot[THREE_OF_A_KIND]:
+            igot[5] = i
+
+        elif acc == 2 and i > igot[PAIR]:
+            igot[7] = i
+
+    # look for flush
+    for i in suit_accumilations:
+        if i >= 5:
+            igot[FLUSH] = i
+
+    # look for streight
+    iterer = 0
+    for i, c in enumerate(rank_accumulations):
+        if c:
+            iterer += 1
+            if iterer >= 5:
+                igot[STREIGHT] = i
+        else:
+            iterer = 0
+
+    # look for streight flush
+    #TODO: account for the 5 card limit if the flush and streight use different cards
+    if igot[STREIGHT] >= 0 and igot[FLUSH] >= 0:
+        igot[STREIGHT_FLUSH] = igot[STREIGHT]
+
+    # look for full house
+    #TODO: account for the 5 card limie
+    if accumulation_counts[2] + accumulation_counts[3] >= 2:
+        igot[FULL_HOUSE] = igot[THREE_OF_A_KIND]
+
+    # look for two pair
+    if accumulation_counts[2] >= 2:
+        igot[TWO_PAIR] = igot[PAIR]
+
+    # high card
+    ## TODO: account for cards unable to be counted towards this due to 5 card max
+    igot[HIGH_CARD_1] = max(cards[0][1], cards[1][1])
+    igot[HIGH_CARD_2] = min(cards[0][1], cards[1][1])
+
+    return igot
